00a8336898f5d6c552750f2d45b200618d72b4fb
Jose-ant-1 <jgonsol1510@g.educaand.es>
2026-02-17 08:25:48 +0100

    primer comit

---
+++ src/main/java/com/page/api_uma/ApiUmaApplication.java
@@ -0,0 +1,13 @@
+package com.page.api_uma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/page/api_uma/ApiUmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.page.api_uma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
